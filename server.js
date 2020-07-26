3b8214efd2de319527845e78366bcc5f013fcfec
Indrakant Mishra <39933769+indrakantm23@users.noreply.github.com>
2020-07-26 18:48:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use(bodyParser.json());
 // mongodb+srv://indrakant:Vishal@123@cookitabhicluster.ppui1.mongodb.net/cookitabhi?retryWrites=true&w=majority
 
 // mongodb://127.0.0.1:27017/cookitabhi
-// process.env.MONGODB_URI || 
-mongoose.connect('mongodb+srv://indrakant:Vishal@123@cookitabhicluster.ppui1.mongodb.net/cookitabhi?retryWrites=true&w=majority', { useNewUrlParser: true });
+// process.env.db || 
+mongoose.connect(process.env.db, { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', ()=> {
